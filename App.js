3beacee9123741e53263cbdc3c47bfd6f6fabc96
Henrique <hiknapolitano@gmail.com>
2020-01-28 23:02:11 -0300

    it looks good and works as expected

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Image } from 'react-native';
 import './GlobalStyles';
 import { TextInputMask } from 'react-native-masked-text'
 import GlobalStyles from './GlobalStyles';
@@ -16,37 +16,43 @@ export default function App() {
   <>
     {/* <StatusBar barStyle="dark-content" backgroundColor="#330066" /> */}
       <View style={styles.container}>
+          <View style={styles.iconContainer}>
+              <Image style={styles.icon} source={require("./assets/gas-sign.png")}></Image>
+              <Text style={styles.initialText}>Digite o preço por litro da gasolina e do etanol em sua cidade, e descubra 
+              qual está mais vantajoso no momento. Economize dinheiro.</Text>
+          </View>
       <View style={styles.bothPricesContainer} >
-      <View style={styles.block}>
-        <Text style={styles.mainText}>Preço da gasolina:</Text>
-        <TextInputMask
-          style={styles.input}
-          maxLength={7}
-          type={'money'}
-          value={gasValue}
-          includeRawValueInChangeText={true}
-          onChangeText={(text, rawText) => {
-            setGasValue(rawText);
-          }}/>
+          <View style={styles.block}>
+                <Text style={styles.mainText}>Preço da gasolina:</Text>
+                <TextInputMask
+                  style={styles.input}
+                  maxLength={7}
+                  type={'money'}
+                  value={gasValue}
+                  includeRawValueInChangeText={true}
+                  onChangeText={(text, rawText) => {
+                    setGasValue(rawText);
+                  }}/>
+          </View>
+
+          <View style={styles.block}>
+                <Text style={styles.mainText}>Preço do etanol:</Text>
+                <TextInputMask
+                  style={styles.input}
+                  maxLength={7}
+                  type={'money'}
+                  value={ethValue}
+                  includeRawValueInChangeText={true}
+                  onChangeText={(text, rawText) => {
+                    setEthValue(rawText);
+                  }}/>
+          </View>
        </View>
 
-       <View style={styles.block}>
-        <Text style={styles.mainText}>Preço do etanol:</Text>
-        <TextInputMask
-          style={styles.input}
-          maxLength={7}
-          type={'money'}
-          value={ethValue}
-          includeRawValueInChangeText={true}
-          onChangeText={(text, rawText) => {
-            setEthValue(rawText);
-          }}/>
-       </View>
-       </View>
-
-        <Text style={styles.answerText}> {ethValue  <
-           (70/100 * gasValue) ? "Está compensando o etanol!" : "A gasolina está compensando!"} </Text>
-
+          <View answerContainer>
+              <Text style={styles.answerText}> {gasValue > 0 && ethValue > 0 ? ethValue  <
+                (70/100 * gasValue) ? "Está compensando o etanol!" : "A gasolina está compensando!" : ""} </Text>
+           </View>
       </View>
   </>
   );
@@ -56,10 +62,30 @@ const styles = StyleSheet.create({
 
     block: {
       margin: 20,
+      marginTop: 0,
+    },
+    initialText: {
+      fontSize: 22,
+      marginHorizontal: 70,
+      textAlign: "justify",
+      fontWeight: "700",
+      paddingTop: 50,
+      color: GlobalStyles.colors.oldGray,
+    },
+    iconContainer: {
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    icon: {
+      resizeMode: "contain",
+      width: 120,
+      height: 120,     
+      opacity: 0.8
     },
     bothPricesContainer: {
       margin: 0, 
     },
+
     container: {
     paddingTop: 60,
     justifyContent: "center",
@@ -70,37 +96,55 @@ const styles = StyleSheet.create({
 
   },
   input: {
-    margin: 20,
+    marginHorizontal: 60,
+    marginVertical: 20,
     alignSelf: "stretch",
     justifyContent: "center",
+    fontFamily: "serif",
     textAlign: "center",
     backgroundColor: GlobalStyles.colors.offWhite,
-    borderWidth: 4,
+    borderWidth: 2,
     borderRadius: 50,
-    height: 90,
+    height: 80,
     borderColor: GlobalStyles.colors.almostClear,
-    fontSize: 42,
-    color: GlobalStyles.colors.oldGray
+    fontSize: 32,
+    color: GlobalStyles.colors.red,
+    textShadowColor: GlobalStyles.colors.offRed,
+    textShadowOffset: {width: -1, height: 1},
+    textShadowRadius: 10,
+    fontWeight: "bold",
+
   },
   mainText: {
     color: GlobalStyles.colors.oldGray,
     fontSize: 34,
     alignSelf: "center",
-    fontFamily: "monospace",
-    textShadowColor: GlobalStyles.colors.lightGray,
+    fontFamily: "serif",
+    color: GlobalStyles.colors.oldGray,
+    textShadowOffset: {width: -1, height: 1},
+    textShadowRadius: 4,
+    textShadowColor: 'rgba(0, 30, 0, 0.25)',
     textShadowOffset: {width: -1, height: 1},
     textShadowRadius: 10,
+    fontWeight: "bold",
+
+    
   },
   answerText: {
-    textShadowColor: 'rgba(0, 30, 0, 1)',
+    textShadowColor: 'rgba(55, 255, 55, 0.8)',
     textShadowOffset: {width: -1, height: 1},
     textShadowRadius: 10,
-    margin: 20,
-    fontSize: 44,
-    fontFamily: "monospace",
+    margin: 5,
+    fontSize: 38,
+    fontFamily: "sans-serif",
     fontWeight: "bold",
     color: GlobalStyles.colors.green,
-    justifyContent: "center",
     textAlign: "center"
+  },
+  answerContainer: {
+    flex: 1,
+    justifyContent: "center",
+
   }
+
 });
--- GlobalStyles.js
+++ GlobalStyles.js
@@ -2,17 +2,21 @@
 export default {
   
    colors: {
-     darkYellow: "#f0f0aa",
+     darkYellow: "#a0a033",
      lightYellow: "#faf5d5",
+     yellow: "#f2f2ac",
      darkGray: "#242424",
      lightGray: "#666",
-     offWhite: "#eee",
+     offWhite: "#fefcff",
      red: "#b15e4c",
+     offRed: "#a85e5b88",
      black: "#070500",
      oldBlue: "#b9d2d6",
      gold: "#6c6644",
-     green: "#17d396",
+     green: "#10a366",
      oldGray: "#4e444c",
      almostClear: "rgba(0,0,0,0.1)",
+     blue: "#00378d",
    },
+
   }
